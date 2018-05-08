9f4837a54a6da5ba09a4eb7b04091e8d1b421272
caseydyps <caseydyps@gmail.com>
2018-05-08 20:20:29 +0800

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ##front
+  #root
+  root "posts#index"
+  #user
+  resources :users, only: [:show] do
+    member do
+      get :post
+      get :collect
+      get :comment
+      get :draft
+      get :friend
+    end
+  end
+  #posts
+  resources :posts
+  get "/feeds", to: "feeds#index"
+  ##back
+  #admin
+  namespace :admin do
+    resources :posts, only: [:index, :destroy]
+    resources :users, only: [:index, :update]
+    resources :categories
+  end
 end
